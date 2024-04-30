0c72a466f8a038927ef2a1d7a3ce0099ecbd3b6c
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-04-29 17:08:51 -0700

    add tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { MintRequest } from './suave/mint'
 function App() {
   const [promptInput, setPromptInput] = useState<string>("")
   const [prompts, setPrompts] = useState<string[]>([])
-  const [txHash, setTxHash] = useState<string>()
+  const [suaveTxHash, setSuaveTxHash] = useState<string>()
 
   // TODO: replace with metamask wallet
   const suaveWallet = getSuaveWallet({
@@ -23,7 +23,7 @@ function App() {
       prompts,
     )
     const txHash = await suaveWallet.sendTransaction(mintRequest.confidentialRequest())
-    setTxHash(txHash)
+    setSuaveTxHash(txHash)
   }
 
   const addPrompt = () => {
@@ -32,21 +32,34 @@ function App() {
   }
 
   return (
-    <>
-      <label htmlFor='promptInput'>Prompt</label>
-      <input id='promptInput' type='text' value={promptInput} onChange={e => setPromptInput(e.target.value)} />
-      <button type='button' onClick={addPrompt}>Add Prompt</button>
-      <ul>
-        {prompts.map((prompt, i) => (
-          <li key={`prompt_${i * i}`}>{prompt}</li>
-        ))}
-      </ul>
-      <hr />
-      <button type='button' onClick={sendMintRequest}>Mint NFT</button>
-      <br />
-      <br />
-      {txHash && <div>Transaction Hash: {txHash}</div>}
-    </>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+      <div className="text-2xl font-medium">ChatNFT on SUAVE</div>
+      <div className='container mx-auto app'>
+        <div id="promptArea" className="flex flex-col">
+          <div className='text-lg'>Enter a prompt:</div>
+          <div style={{ width: "100%" }}><input name='promptInput' type='text' value={promptInput} onChange={e => setPromptInput(e.target.value)} /></div>
+          <div style={{ width: "100%" }}><button type='button' onClick={addPrompt}>Add Prompt</button></div>
+        </div>
+        {prompts.length > 0 && <div className="flex flex-col" style={{ width: "100%", alignItems: "flex-start", border: "1px dotted white", padding: 12 }}>
+          <div className='text-xl text-[#f0fff0]'>Your Prompts</div>
+          <div style={{ padding: 32, width: "100%" }} className='flex flex-row'>
+            <div className='basis-1/4'>
+              <button type='button' onClick={sendMintRequest}>Mint NFT</button>
+            </div>
+            <div className='basis-3/4'>
+              <ul className='list-disc' style={{ textAlign: "left" }}>
+                {prompts.map((prompt, i) => (
+                  <li key={`prompt_${i * i}`}>{prompt}</li>
+                ))}
+              </ul>
+            </div>
+            <br />
+            <br />
+          </div>
+          {suaveTxHash && <div>SUAVE Tx Hash: {suaveTxHash}</div>}
+        </div>}
+      </div>
+    </div>
   )
 }
 
